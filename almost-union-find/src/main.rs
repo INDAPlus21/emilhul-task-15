71a2eb62d61a325ff0617a06e395fe337d9445d7
Emil Hultcrantz <emilhtz@gmail.com>
2022-05-21 12:03:34 +0000

    Update almost-union-find and add proof

--- almost-union-find/src/main.rs
+++ almost-union-find/src/main.rs
@@ -1,54 +1,53 @@
-use std::io::{self, BufRead};
+use std::{io::{self, prelude::*}, fmt};
 
 fn main() {
-    // Get input
-    let input = io::stdin();
+    let mut buf: String = String::with_capacity(100);
 
-    let mut lines = input
-        .lock()
-        .lines()
-        .map(|_line| _line.unwrap());
-    
-    // Get first line
-    let values = lines.next().unwrap()
-        .split_whitespace()
-        .map(|_value| _value.parse::<usize>().ok().unwrap())
-        .collect::<Vec<usize>>();
+    // get input lines as strings
+    io::stdin().read_to_string(&mut buf).expect("err");
 
-    if values.len() == 0 { return; }
+    // split by line
+    let lines: Vec<&str> = buf.split('\n').collect();
 
-    let (n, m) = (values[0], values[1]);
-
-    if n == 0 || m == 0 { return; }
-
-    // Create Almost Union Find datastructure of size n
-    let mut auf = AlmostUnionFind::new(n);
-
-    // For remaining lines in input
-    for _ in 0..m {
-        // Split up line at spaces
-        let line = lines.next().unwrap()
-        .split_whitespace()
-        .map(|_value| _value.parse::<usize>().ok().unwrap())
-        .collect::<Vec<usize>>();
-    
-        // Match first number to function
-        match line[0] {
-            1 => auf.union(line[1], line[2]),
-            2 => auf._move(line[1], line[2]),
-            3 => {
-                let (size, sum) = auf._return(line[1]);
-                println!("{} {}", size, sum);
-            },
-            _ => ()
+    let mut l = 0;
+    while l < lines.len() {
+        if lines[l] == "" {
+            l += 1;
+            continue;
         }
+
+        let mut first = lines[l].split_whitespace();
+
+        let n: usize = first.next().unwrap().parse().unwrap();
+        let m: usize = first.next().unwrap().parse().unwrap();
+
+        let mut auf = AlmostUnionFind::new(n);
+
+        for i in (l + 1)..(l + m + 1) {
+            let line = lines[i].split_whitespace()
+                .map(|_value| _value.parse::<usize>().ok().unwrap())
+                .collect::<Vec<usize>>();
+
+            // Match first number to function
+            match line[0] {
+                1 => auf.union(line[1], line[2]),
+                2 => auf._move(line[1], line[2]),
+                3 => {
+                    let (size, sum) = auf._return(line[1]);
+                    println!("{} {}", size, sum);
+                },
+                _ => panic!(),
+            }
+        }
+        l += m + 1;
     }
 }
 
 #[derive(Debug)]
 /// ## AlmostUnionFind
-/// A datastructure that's almost union finde :)
+/// A datastructure that's almost union find :)
 struct AlmostUnionFind {
+    n: usize,
     /// The size of each set in the Almost Union-Find
     set_size: Vec<usize>,
     /// The id of each set in the Almost Union-Find.
@@ -66,7 +65,7 @@ impl AlmostUnionFind {
         let mut set_id = vec![0;2*(n+1)];
         let mut set_sum = vec![0;2*(n+1)];
 
-        let mut j = n+1;
+        let mut j = n+2;
 
         for i in 1..=n {
             set_id[i] = j;
@@ -75,7 +74,7 @@ impl AlmostUnionFind {
             j += 1;
         }
 
-        AlmostUnionFind {  set_size, set_id, set_sum }
+        AlmostUnionFind {  n, set_size, set_id, set_sum }
     }
 
     /// ## find
@@ -136,5 +135,11 @@ impl AlmostUnionFind {
         let size = self.set_size[root_p];
         let sum = self.set_sum[root_p];
         (size, sum)
+    }
+}
+
+impl fmt::Display for AlmostUnionFind {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Set Size: {:?}\nSet Id: {:?}\nSet Bu: {:?}\nSet Sum: {:?}", &self.set_size[self.n+2..], &self.set_id[1..self.n+1], &self.set_id[self.n+2..], &self.set_sum[self.n+2..])
     }
 }
